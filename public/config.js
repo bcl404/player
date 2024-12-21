59127d7fd0be28ce3ed627da1f7df98d2c16a922
bcl404 <srvcsr@gmail.com>
2024-12-21 08:30:42 +0700

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
   // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "https://kuli.noads.workers.dev/",
+  VITE_CORS_PROXY_URL: "https://kuli.noads.workers.dev",
 
   // The READ API key to access TMDB
   VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZWZkNWU0M2I0MmI4ZGM3OGVkM2ExMWQ4MDNkOGIzNyIsIm5iZiI6MTczNDc0Mjk3OC45NzMsInN1YiI6IjY3NjYxM2MyMzMwYmNlNmVjOTkwZWRkOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FMmh6LmZKmBf5hG00SKlZCuql5ffbXyr_3i5xn8YxSA",
@@ -13,7 +13,7 @@ window.__CONFIG__ = {
   VITE_NORMAL_ROUTER: true,
 
   // The backend URL to communicate with
-  VITE_BACKEND_URL: "backend-production-ceb8.up.railway.app",
+  VITE_BACKEND_URL: "https://backend-production-ceb8.up.railway.app",
 
   // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
   VITE_DISALLOWED_IDS: "",
